105796fb8ade8a2ca48aaee6123b029389d8cb10
Lars Juul <ljb@easv.dk>
2021-11-08 12:18:51 +0100

    Adding products module and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', loadChildren: () =>
+    import('./products/products.module')
+      .then(f => f.ProductsModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
